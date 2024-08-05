14b22ea9b59a2b125c91a1e2bada751e6bb71965
rhysfx <rhysytbro@gmail.com>
2024-08-05 18:03:07 +0100

    remove car ability?

--- resources/[aura]/aura-trains/lua/client.lua
+++ resources/[aura]/aura-trains/lua/client.lua
@@ -2,8 +2,6 @@ Trains.Client = {}
 
 RegisterCommand('spawntrain', function()
     local trainModel = 'freight'
-    local trainCarModel = 'freightcar'
-
     local playerPed = PlayerPedId()
     local playerPos = GetEntityCoords(playerPed)
 
@@ -11,19 +9,6 @@ RegisterCommand('spawntrain', function()
     while not HasModelLoaded(GetHashKey(trainModel)) do
         Wait(1)
     end
-
-    RequestModel(GetHashKey(trainCarModel))
-    while not HasModelLoaded(GetHashKey(trainCarModel)) do
-        Wait(1)
-    end
-
     local train = CreateMissionTrain(24, playerPos.x, playerPos.y, playerPos.z, true)
-
-    local carCount = 2
-    for i = 1, carCount do
-        local car = CreateVehicle(GetHashKey(trainCarModel), playerPos.x, playerPos.y, playerPos.z, 0.0, true, true)
-        AttachVehicleToTrain(car, train, i)
-    end
-
     TaskWarpPedIntoVehicle(playerPed, train, -1)
 end, false)
