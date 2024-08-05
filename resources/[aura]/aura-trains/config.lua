88fdc16b64dba8115c10812f43915a6afeab2393
rhysfx <rhysytbro@gmail.com>
2024-08-05 17:45:13 +0100

    big stuff

---
+++ resources/[aura]/aura-trains/config.lua
@@ -0,0 +1,2 @@
+Trains = {}
+Trains.Config = {}
---
+++ resources/[aura]/aura-trains/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'bodacious'
+games {'gta5'}
+
+shared_scripts {
+	'config.lua'
+}
+
+client_scripts {
+	'lua/client.lua'
+}
+
+server_scripts {
+	'lua/server.lua'
+}
